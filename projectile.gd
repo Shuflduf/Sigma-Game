f73c98d3b57b97d65c6ae1853d9d817e246b3db6
Shuflduf <shuflduf@gmail.com>
2026-04-23 16:17:40 -0600

    update lights

--- projectile.gd
+++ projectile.gd
@@ -2,6 +2,9 @@ extends RigidBody3D
 
 
 func _physics_process(delta: float) -> void:
-	#print(linear_velocity)
 	if not linear_velocity.is_zero_approx():
 		linear_velocity = linear_velocity.normalized() * 10.0
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
